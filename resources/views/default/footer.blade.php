ccef4b7b3da8e87156447bc324d78995fdca71c5
yjc <yjc@6b4743de-5cd4-bb44-8f5e-897828f205ad>
2018-04-03 03:45:36 +0000

    git-svn-id: https://39.108.144.9:8443/svn/hotword@18 6b4743de-5cd4-bb44-8f5e-897828f205ad

--- resources/views/default/footer.blade.php
+++ resources/views/default/footer.blade.php
@@ -17,7 +17,7 @@
 		wx.onMenuShareAppMessage({
 			  title: $("title").text(), // 分享标题
 			  desc: '"'+des+'"', // 分享描述
-			  link: 'http://word.feibu.info', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			  link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
 			  imgUrl: 'http://word.feibu.info/default/images/logo.png', // 分享图标
 			  type: 'link', // 分享类型,music、video或link，不填默认为link
 			  dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
@@ -31,7 +31,7 @@
 		  });
 		wx.onMenuShareTimeline({
 			title: $("title").text()+des, // 分享标题
-			link: 'http://word.feibu.info', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
 			imgUrl: 'http://word.feibu.info/default/images/logo.png', // 分享图标
 			success: function () { 
 				// fb_toast("分享成功");
