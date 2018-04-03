91bbebee0b91226f6d6c72328c57d39743405a5c
yjc <yjc@6b4743de-5cd4-bb44-8f5e-897828f205ad>
2018-04-03 03:49:29 +0000

    git-svn-id: https://39.108.144.9:8443/svn/hotword@19 6b4743de-5cd4-bb44-8f5e-897828f205ad

--- resources/views/default/footer.blade.php
+++ resources/views/default/footer.blade.php
@@ -13,7 +13,7 @@
 		  //分享任务
 	wx.ready(function(){
 		var des = $(".markdown-body").length == 0 ? $("meta[name=description]").attr("content") : $(".markdown-body").find("p").text();
-		console.log(des)
+		des = des.length > 50 ? des.substring(0,50) : des;
 		wx.onMenuShareAppMessage({
 			  title: $("title").text(), // 分享标题
 			  desc: '"'+des+'"', // 分享描述
