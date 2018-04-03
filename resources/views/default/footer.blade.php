ad93f6ba9ab072f1695e8c84f696cc7cfb89d00e
yjc <yjc@6b4743de-5cd4-bb44-8f5e-897828f205ad>
2018-04-03 03:32:25 +0000

    git-svn-id: https://39.108.144.9:8443/svn/hotword@12 6b4743de-5cd4-bb44-8f5e-897828f205ad

--- resources/views/default/footer.blade.php
+++ resources/views/default/footer.blade.php
@@ -9,4 +9,36 @@
         </div>
     </div>
 </footer>
+<script>
+		  //分享任务
+	wx.ready(function(){
+		var des = $(".markdown-body").length == 0 ? $($("meta[name=description]").attr("content")) : $(".markdown-body").find("p").text();
+		wx.onMenuShareAppMessage({
+			  title: $("title").text(), // 分享标题
+			  desc: des, // 分享描述
+			  link: 'http://word.feibu.info', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			  imgUrl: 'http://word.feibu.info/default/images/logo.png', // 分享图标
+			  type: 'link', // 分享类型,music、video或link，不填默认为link
+			  dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+			  success: function () { 
+				  fb_toast("分享成功");
+				  
+			  },
+			  cancel: function () { 
+				fb_toast("取消分享", "cancel");
+			  } 
+		  });
+		wx.onMenuShareTimeline({
+			title: $("title").text()+des, // 分享标题
+			link: 'http://word.feibu.info', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			imgUrl: 'http://word.feibu.info/default/images/logo.png', // 分享图标
+			success: function () { 
+				fb_toast("分享成功");
+			},
+			cancel: function () { 
+				fb_toast("取消分享", "cancel");
+			}
+		});
+	})
+</script>
 {!! $systemPresenter->getKeyValue('statistics_script') !!}
