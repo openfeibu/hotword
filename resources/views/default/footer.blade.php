a1680f1f260e04d06637189eba1f901cada6b6ae
yjc <yjc@6b4743de-5cd4-bb44-8f5e-897828f205ad>
2018-04-03 03:41:48 +0000

    git-svn-id: https://39.108.144.9:8443/svn/hotword@17 6b4743de-5cd4-bb44-8f5e-897828f205ad

--- resources/views/default/footer.blade.php
+++ resources/views/default/footer.blade.php
@@ -22,11 +22,11 @@
 			  type: 'link', // 分享类型,music、video或link，不填默认为link
 			  dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
 			  success: function () { 
-				  fb_toast("分享成功");
+				  // fb_toast("分享成功");
 				  
 			  },
 			  cancel: function () { 
-				fb_toast("取消分享", "cancel");
+				// fb_toast("取消分享", "cancel");
 			  } 
 		  });
 		wx.onMenuShareTimeline({
@@ -34,10 +34,10 @@
 			link: 'http://word.feibu.info', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
 			imgUrl: 'http://word.feibu.info/default/images/logo.png', // 分享图标
 			success: function () { 
-				fb_toast("分享成功");
+				// fb_toast("分享成功");
 			},
 			cancel: function () { 
-				fb_toast("取消分享", "cancel");
+				// fb_toast("取消分享", "cancel");
 			}
 		});
 	})
